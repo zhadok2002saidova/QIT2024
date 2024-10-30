6e9a305152c650f14a56e7c2568dc5a6b6c4898f
Zhadyra <zhadyratalgatovna1006@gmail.com>
2024-10-30 12:58:55 +0500

    ex done

---
+++ examples/const.js
@@ -0,0 +1,13 @@
+var name = "Aty"; // өзгеретін мән
+
+let surname = "Familiya"; // Ozgeredi
+
+surname = "ABC"; // ainymalynyn mani ozgeredi
+
+const id = 909090 ; // turaqty man
+
+// id= 84938498 ; // qate qaitarady
+
+console.log(id); // shykpaidy
+
+let faculties = ["medicine" ,"sport",  "law", ] //array
