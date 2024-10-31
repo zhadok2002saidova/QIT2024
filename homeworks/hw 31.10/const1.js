2eca8692ecae31aa97089cf4393a8c5d8d71155c
Zhadyra <zhadyratalgatovna1006@gmail.com>
2024-10-31 19:17:02 +0500

    hw 31.10 done

---
+++ homeworks/hw 31.10/const1.js
@@ -0,0 +1,18 @@
+let slicesPerPizza = 8;
+let numberOfPizzas = 5;
+
+let result = (slicesPerPizza*numberOfPizzas)
+console.log(result);
+
+ let totalChocolates = 24;
+ let numberOfFriends = 6;
+
+ let chocolatesPerFriend = (totalChocolates / numberOfFriends)
+ console.log ( chocolatesPerFriend) ;
+ 
+ let plantHeight = 10;
+ plantHeight += 2;
+
+ console.log (plantHeight) ;
+ 
+
