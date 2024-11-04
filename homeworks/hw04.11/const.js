3d15cee9dd2bb1a0e9770e2e484376e6308ba2a7
Zhadyra <zhadyratalgatovna1006@gmail.com>
2024-11-04 18:52:38 +0500

    hw 04.11 done

---
+++ homeworks/hw04.11/const.js
@@ -0,0 +1,35 @@
+var age = 22; // 1 tapsyrma
+
+if (age > 16) {
+  console.log("zhasy 16 dan asty");
+} else {
+  console.log("zhasy 16 dan aspady");
+}
+
+var num = 14; // 2 tapsyrma
+
+if (num % 5 == 0) {
+  console.log("zhup san");
+} else {
+  console.log("tak san");
+}
+
+var temperature = 28; // 3 tapsyrma
+
+if (temperature >= 20 && temperature <= 30) {
+  console.log("30-20 graduska deiyn.");
+}
+
+var num = 22; // 4 tapsyrma
+
+if (num > 0) {
+  console.log("on san");
+}
+
+var num = 22; // 5 tapsyrma
+
+if (num % 5 == 0 && num % 10 == 0) {
+  console.log("shyn");
+} else {
+  console.log("zhalgan");
+}
