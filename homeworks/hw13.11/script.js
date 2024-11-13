01c04cb703893d2d3278a995504197dd61e2d87b
Zhadyra <zhadyratalgatovna1006@gmail.com>
2024-11-13 19:51:29 +0500

    hw 13.11 done

--- homeworks/hw13.11/script.js
+++ homeworks/hw13.11/script.js
@@ -2,15 +2,15 @@
 var num = 1;
 
 while (num <= 20) {
-  if (num % 2 === 0) {
+  if (num % 2 == 0) {
     num++;
     continue;
   }
-  if (num % 3 === 0 && num % 5 === 0) {
+  if (num % 3 == 0 && num % 5 === 0) {
     console.log(num + ": FizzBuzz");
-  } else if (num % 3 === 0) {
+  } else if (num % 3 == 0) {
     console.log(num + ": Fizz");
-  } else if (num % 5 === 0) {
+  } else if (num % 5 == 0) {
     console.log(num + ": Buzz");
   } else {
     console.log(num);
